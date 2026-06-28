7167db3fed1246dc5f829acbf4f25368eb0e7a56
josmacpac <josmacpac@gmail.com>
2026-06-27 23:04:58 -0600

    se cambian estilos a tailwind

---
+++ mypetcare/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'powder-blush': '#FFA69E',
+        'alice-blue': '#edf6f9',
+        'pearl-agua': '#83c5be',
+        'light-blue': '#AED9E0',
+        'blue-slate': '#5E6472',
+        'tangerine-dream': '#e29578',
+      }
+    }
+  }
+}
